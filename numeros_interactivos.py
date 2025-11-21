6c6d5eb5f6935d1f1e7fa01e4a57abddc9669352
maxiherrera01 <maxi.herrera.a01@gmail.com>
2025-11-21 15:45:42 +0000

    Agregar operaciones matemáticas interactivas en numeros_interactivos.py

---
+++ numeros_interactivos.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Ingresa el primer número: "))
+
+numero2 = int(input("Ingresa el segundo número: "))
+
+print(f"La suma es: {numero1} y {numero2} es {numero1 + numero2}")
+print(f"La resta es: {numero1} menos {numero2} es {numero1 - numero2}")
+print(f"La multiplicación es: {numero1} por {numero2} es {numero1 * numero2}")
+print(f"La división es: {numero1} dividido {numero2} es {numero1 / numero2}")   
+print(f"El módulo es: {numero1} módulo {numero2} es {numero1 % numero2}")  
+print(f"La potencia es: {numero1} elevado a {numero2} es {numero1 ** numero2}") 
+
+
