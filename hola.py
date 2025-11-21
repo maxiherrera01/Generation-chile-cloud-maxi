dfc7a9114e893aeda949188e79bc828fea00a441
maxiherrera01 <maxi.herrera.a01@gmail.com>
2025-11-21 14:53:22 +0000

    creacion de archivos y numeros

---
+++ hola.py
@@ -0,0 +1,8 @@
+# tu nombre
+# su ciudad
+# un emogi
+
+
+print ("Maximiliano Herrera")
+print ("San felipe")
+print ("👽")
---
+++ numeros.py
@@ -0,0 +1,10 @@
+entero = 10 # número entero
+decimal = 3.14 # número decimal
+suma = entero + 5
+modulo = entero % 2
+
+print("La suma es:", suma)
+print("El número decimal es:", decimal)
+print ("division", ((decimal / 3)+2))
+print ("potencia:", (entero ** 3))
+print ("modulo:", modulo)
